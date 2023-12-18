d6cc4541a1b82d9e0255c5b2b7e68bbe79558f94
Nadjide.O <106732960+Nadjide@users.noreply.github.com>
2023-12-18 09:44:56 +0100

    ajout des viewset

--- api/serializers.py
+++ api/serializers.py
@@ -1,14 +1,20 @@
-from rest_framework import serializers
-
-
+from rest_framework import serializers, viewsets
 from .models import Concessionnaire, Voiture
 
 class ConcessionnaireSerializer(serializers.ModelSerializer):
     class Meta:
         model = Concessionnaire
         fields = ('id', 'nom', 'code_postal')
-        
+
 class VoitureSerializer(serializers.ModelSerializer):
     class Meta:
         model = Voiture
         fields = '__all__'
+
+class ConcessionnaireViewSet(viewsets.ModelViewSet):
+    queryset = Concessionnaire.objects.all()
+    serializer_class = ConcessionnaireSerializer
+
+class VoitureViewSet(viewsets.ModelViewSet):
+    queryset = Voiture.objects.all()
+    serializer_class = VoitureSerializer
