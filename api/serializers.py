312b1c6e1ef2626a30479b4e8c424de85a760067
Nadjide.O <106732960+Nadjide@users.noreply.github.com>
2023-12-18 09:44:03 +0100

    ajout des serializer pour le concess et la voiture

---
+++ api/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+
+
+from .models import Concessionnaire, Voiture
+
+class ConcessionnaireSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Concessionnaire
+        fields = ('id', 'nom', 'code_postal')
+        
+class VoitureSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Voiture
+        fields = '__all__'
